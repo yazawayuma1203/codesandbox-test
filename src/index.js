f0a66e429fa2616dd42a2ec614e48a6f646f7859
yazawayuma1203 <110238678+yazawayuma1203@users.noreply.github.com>
2022-07-29 23:12:36 +0900

    分割代入 分割代入を用いる方法

--- src/index.js
+++ src/index.js
@@ -70,14 +70,40 @@
 //   return str;
 // }
 
-const func1 = function (str) {
-  return str;
-};
-console.log(func1("func1です"));
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
 
-//アロー関数
-const func2 = (str) => str;
-console.log(func2("func2です"));
+// //アロー関数
+// const func2 = (str) => str;
+// console.log(func2("func2です"));
 
-const func3 = (num1, num2) => num1 + num2;
-console.log(func3(10, 20));
+// const func3 = (num1, num2) => num1 + num2;
+// console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+
+// const myProfile = {
+//   name: "矢澤佑真",
+//   age: 24
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+//const myProfile = ["矢澤佑真", 24];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
