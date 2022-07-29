518d1c5fa34575796a4cb21e4a3d13d462f37796
yazawayuma1203 <110238678+yazawayuma1203@users.noreply.github.com>
2022-07-29 23:01:31 +0900

    アロー関数 アロー関数を用いる方法

--- src/index.js
+++ src/index.js
@@ -50,13 +50,34 @@
 /**
  * テンプレート文字列
  */
-const name = "矢澤佑真";
-const age = 24;
+// const name = "矢澤佑真";
+// const age = 24;
 
-//「私の名前は矢澤佑真です。年齢は24歳です。」
-const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
-console.log(message1);
+// //「私の名前は矢澤佑真です。年齢は24歳です。」
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+// console.log(message1);
 
-//テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
-console.log(message2);
+// //テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+
+//従来の関数
+// function func1(str) {
+//   return str;
+// }
+
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("func1です"));
+
+//アロー関数
+const func2 = (str) => str;
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => num1 + num2;
+console.log(func3(10, 20));
